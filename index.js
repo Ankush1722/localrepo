8967e7c424551bb964e48d8f3fd58c57e8cdf634
Ankush1722 <88156996+Ankush1722@users.noreply.github.com>
2024-06-19 15:54:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const wikip = require('wiki-infobox-parser');
+const port = 3000;
 
 //ejs
 app.set("view engine", 'ejs');
@@ -50,4 +51,4 @@ app.get('/index', (req,response) =>{
 });
 
 //port
-app.listen(3000, console.log("Listening at port 3000..."))
+app.listen(port, console.log("Listening at port 3000..."))
